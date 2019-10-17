50433b59bd3c7d7b494dca8073f89c5ea4f9c827
bodhibrdr <julie.gumerman@gmail.com>
2019-10-17 14:47:53 -0600

    addStuff test works, get fails

--- funstuff/funstuff.spec.js
+++ funstuff/funstuff.spec.js
@@ -1,15 +1,34 @@
-const Stuff = require("./funstuff-model.js")
-const db = require("../data/dbConfig.js")
+const Stuff = require("./funstuff-model.js");
+const request = require("supertest");
+const stuffRoute = require("./funstuff-route");
+const db = require("../data/dbConfig.js");
 
 // describe("funstuff model", () => {
 
 // })
 
-describe("addStuff", () => {
+describe("stuff model", () => {
+    beforeEach(async () => {
+        await db("funstuff").truncate();
+    })
+
+    describe("addStuff", () => {
     it ("should add a fun thing to the database", async () => {
-        await Stuff.addStuff({ fun_thing: "milkshakes"})
+        await Stuff.addStuff({ fun_thing: "rainstorms"})
         const stuff = await db("funstuff");
         let i = stuff.length;
         expect(stuff).toHaveLength(i++);
+    })
+
+    })
+})
+
+describe("/ get", () => {
+    it("should return a 200 status code on completion", () => {
+        return request(stuffRoute)
+            .get("/")
+            .then(res => {
+                expect(res.status).toBe(200);
+            })
     })
 })
