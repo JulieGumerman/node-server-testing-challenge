4953fe74ce1ed7c56115378a19607f8165933177
bodhibrdr <julie.gumerman@gmail.com>
2019-10-17 13:47:13 -0600

    can get items and possibly add an item

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const server = express();
 
+const stuffRoute = require("./funstuff/funstuff-route");
+
 server.use(express.json());
+server.use("/api/funstuff", stuffRoute);
 
 const port = 3333;
 
